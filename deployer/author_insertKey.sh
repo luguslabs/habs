787f89ab3b264451d3ee36f37fdb1694e6138a3f
francois branciard <francoisbranciard@protonmail.com>
2020-01-20 14:38:00 +0100

    [deployer] insert both format aura and grandpa keys

--- deployer/author_insertKey.sh
+++ deployer/author_insertKey.sh
@@ -14,18 +14,29 @@ then
       exit 1
 fi
 
-ARCHIPEL_PUBLIC_KEY=$(subkey inspect "$ARCHIPEL_KEY_SEED" | grep Public | cut -d":" -f2 | sed -e 's/^[[:space:]]*//')
+ARCHIPEL_PUBLIC_KEY_ED25519=$(subkey --ed25519 inspect "$ARCHIPEL_KEY_SEED" | grep Public | cut -d":" -f2 | sed -e 's/^[[:space:]]*//')
 
-if [ -z "$ARCHIPEL_PUBLIC_KEY" ]
+
+if [ -z "$ARCHIPEL_PUBLIC_KEY_ED25519" ]
 then
-      echo "\$ARCHIPEL_PUBLIC_KEY no found using subkey"
+      echo "\$ARCHIPEL_PUBLIC_KEY_ED25519 no found using subkey"
       exit 1
 fi
 
+ARCHIPEL_PUBLIC_KEY_SR25519=$(subkey --sr25519 inspect "$ARCHIPEL_KEY_SEED" | grep Public | cut -d":" -f2 | sed -e 's/^[[:space:]]*//')
+
+if [ -z "$ARCHIPEL_PUBLIC_KEY_SR25519" ]
+then
+      echo "\$ARCHIPEL_PUBLIC_KEY_SR25519 no found using subkey"
+      exit 1
+fi
+
+echo "ARCHIPEL_PUBLIC_KEY_ED25519 is $ARCHIPEL_PUBLIC_KEY_ED25519"
+echo "ARCHIPEL_PUBLIC_KEY_SR25519 is $ARCHIPEL_PUBLIC_KEY_SR25519"
 
 while [ true ]
 do
-  echo "---------author_insertKey-----------"
+  echo "---------author_insertKey GRANDPA-----------"
   curl http://localhost:9933 -s -H "Content-Type:application/json;charset=utf-8" -d \
   '{
     "jsonrpc":"2.0",
@@ -34,7 +45,21 @@ do
     "params": [
       "aura",
        "'"$ARCHIPEL_KEY_SEED"'",
-       "'"$ARCHIPEL_PUBLIC_KEY"'"
+       "'"$ARCHIPEL_PUBLIC_KEY_ED25519"'"
+    ]
+  }'
+  echo "---------------------------"
+
+  echo "---------author_insertKey AURA-----------"
+  curl http://localhost:9933 -s -H "Content-Type:application/json;charset=utf-8" -d \
+  '{
+    "jsonrpc":"2.0",
+    "id":1,
+    "method":"author_insertKey",
+    "params": [
+      "aura",
+       "'"$ARCHIPEL_KEY_SEED"'",
+       "'"$ARCHIPEL_PUBLIC_KEY_SR25519"'"
     ]
   }'
   echo "---------------------------"
