930c540c411f4daa4a0c0d2056f6717ecde7bf36
Vladimir <vladost.pro@gmail.com>
2020-01-24 12:22:24 +0100

    [deployer] correct remove file

--- deployer/test/remove.sh
+++ deployer/test/remove.sh
@@ -4,7 +4,9 @@ docker rm -f archipel-ui
 docker stop archipel{1,2,3,-node}
 docker rm archipel{1,2,3,-node}
 
-cd test/orchestrator && bash remove.sh
-cd ../.. && sudo rm -R archipel{1,2,3,-node}
+cd orchestrator && bash remove.sh
+cd .. && sudo rm -R archipel{1,2,3,-node}
+
+docker network rm archipel
 
 docker ps
