b34d2c020d4c5237bb2a62a05d4a9661f1eb75a2
francois branciard <francoisbranciard@protonmail.com>
2020-02-14 16:56:18 +0100

    add utils js to generate node_key, peer id

---
+++ deployer/configutils/generateNewNodeKeys.js
@@ -0,0 +1,10 @@
+const PeerId = require('peer-id')
+
+const main = async function(a, b) {
+    const id = await PeerId.create({ bits: 1024, keyType: 'rsa' });
+    //console.log(JSON.stringify(id.toJSON(), null, 2));
+    console.log("PEER_ID="+id.toB58String());
+    console.log("NODE_KEY="+id.toHexString());
+}
+  
+main();
