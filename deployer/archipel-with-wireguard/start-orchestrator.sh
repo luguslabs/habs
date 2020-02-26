5bca68fe909d11fd676b2921445121d39915210a
francois branciard <francoisbranciard@protonmail.com>
2020-02-26 21:51:09 +0100

    #53 add tempo for orchestrator. #52 add vpn for polkadot test too

--- deployer/archipel-with-wireguard/start-orchestrator.sh
+++ deployer/archipel-with-wireguard/start-orchestrator.sh
@@ -4,6 +4,8 @@ export NODE_ENV=production
 export NODE_WS="ws://127.0.0.1:9944"
 export MNEMONIC="$ARCHIPEL_KEY_SEED"
 export ALIVE_TIME=60000
-
+echo "sleep 5 sec to wait archipel node started and ws endpoint ready..."
+sleep 5
+echo "Launch orchestrator"
 # Launch orchestrator
 exec node src/app.js
