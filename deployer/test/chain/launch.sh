431d0bc93e940537ac39d476a34ebb13c80acb34
francois branciard <francoisbranciard@protonmail.com>
2020-03-03 20:46:15 +0100

    mock isServiceReadyToStart fn for tests

--- deployer/test/chain/launch.sh
+++ deployer/test/chain/launch.sh
@@ -10,7 +10,7 @@ function launch_node () {
     --ip "$2" \
     --env ARCHIPEL_NODE_ALIAS=$1 \
     --env ARCHIPEL_KEY_SEED="$4" \
-    --env ARCHIPEL_CHAIN_ADDITIONAL_PARAMS="$5" \
+    --env ARCHIPEL_RESERVED_PEERS="$5" \
     --env ARCHIPEL_AUTHORITIES_SR25519_LIST="5FmqMTGCW6yGmqzu2Mp9f7kLgyi5NfLmYPWDVMNw9UqwU2Bs,5H19p4jm177Aj4X28xwL2cAAbxgyAcitZU5ox8hHteScvsex,5DqDvHkyfyBR8wtMpAVuiWA2wAAVWptA8HtnsvQT7Uacbd4s" \
     --env ARCHIPEL_AUTHORITIES_ED25519_LIST="5FbQNUq3kDC9XHtQP6iFP5PZmug9khSNcSRZwdUuwTz76yQY,5GiUmSvtiRtLfPPAVovSjgo6NnDUDs4tfh6V28RgZQgunkAF,5EGkuW6uSqiZZiZCyVfQZB9SKw5sQc4Cok8kP5aGEq3mpyVj" \
     luguslabs/archipel-chain:$ARCHIPEL_CHAIN_VERSION
@@ -69,9 +69,9 @@ echo "Local node2 identity is '$NODE2_LOCAL_ID'"
 get_node_identity "node3" NODE3_LOCAL_ID
 echo "Local node3 identity is '$NODE3_LOCAL_ID'"
 
-# Constructing bootnodes list
-BOOTNODES_LIST="--bootnodes /ip4/$NODE1_IP/tcp/30333/p2p/$NODE1_LOCAL_ID --bootnodes /ip4/$NODE2_IP/tcp/30333/p2p/$NODE2_LOCAL_ID --bootnodes /ip4/$NODE3_IP/tcp/30333/p2p/$NODE3_LOCAL_ID"
-echo "Bootnodes list is '$BOOTNODES_LIST'"
+# Constructing reserved  list
+RESERVED_LIST="/ip4/$NODE1_IP/tcp/30333/p2p/$NODE1_LOCAL_ID,/ip4/$NODE2_IP/tcp/30333/p2p/$NODE2_LOCAL_ID,/ip4/$NODE3_IP/tcp/30333/p2p/$NODE3_LOCAL_ID"
+echo "RESERVED_LIST  is '$RESERVED_LIST'"
 
 echo "Sleeping 10 sec before node remove to be sure that keys were added..."
 sleep 10
@@ -88,21 +88,21 @@ launch_node "node1" \
             "$NODE1_IP" \
             "-p 9944:9944 -p 9933:9933 -p 9955:9955" \
             "mushroom ladder bomb tornado clown wife bean creek axis flat pave cloud" \
-            "$BOOTNODES_LIST"
+            "$RESERVED_LIST"
 
 # Starting node2
 launch_node "node2" \
             "$NODE2_IP" \
             "" \
             "fiscal toe illness tunnel pill spatial kind dash educate modify sustain suffer" \
-            "$BOOTNODES_LIST"
+            "$RESERVED_LIST"
 
 # Starting node3
 launch_node "node3" \
             "$NODE3_IP" \
             "" \
             "borrow initial guard hunt corn trust student opera now economy thumb argue" \
-            "$BOOTNODES_LIST"
+            "$RESERVED_LIST"
 
 echo "Chain is fully initilized!"
 docker ps
