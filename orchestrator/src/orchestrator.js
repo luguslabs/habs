e6ef646dc7d46fafdce59456440abcd64058ba99
Vladimir <vladost.pro@gmail.com>
2020-03-13 11:50:07 +0100

    [orchestrator] Refactor orchestrating mechanism

--- orchestrator/src/orchestrator.js
+++ orchestrator/src/orchestrator.js
@@ -42,42 +42,108 @@ class Orchestrator {
 
       // If node state permits to send transactions
       const sendTransaction = await this.chain.canSendTransactions();
-
-      if (sendTransaction) {
-        console.log('Archipel node has some peers and is synchronized so orchestrating...');
-        // Get node address from seed
-        const key = await getKeysFromSeed(this.mnemonic);
-        const nodeKey = key.address;
-        debug('orchestrateService', `Current Node Key: ${nodeKey}`);
-
-        // Get current leader from chain
-        let currentLeader = await this.chain.getLeader();
-        currentLeader = currentLeader.toString();
-
-        // If current leader is already set
-        if (currentLeader !== '') {
-          debug('orchestrateService', `Current Leader: ${currentLeader}`);
-          // If you are the current leader
-          if (currentLeader === nodeKey) {
-            console.log('Current node is leader.');
-            await this.serviceStartIfAnyoneActive(nodeKey);
-          // If someone else is leader
-          } else {
-            await this.otherLeaderAction(currentLeader, nodeKey);
-          }
-        // Leader is not already set (first time boot)
-        } else {
-          console.log('There is no leader set...');
-          await this.becomeLeader(nodeKey, nodeKey);
-        }
-      } else {
-        console.log('Archipel node can\'t receive transactions...');
+      if (!sendTransaction) {
+        console.log('Archipel chain node can\'t receive transactions...');
+        this.serviceStart('passive');
+        return;
       }
+
+      // Check if service was not suspended
+      if (this.suspendService) {
+        console.log('ARCHIPEL_SUSPEND_SERVICE is set to true... Only Passive mode. Leader will be never launched on this node.');
+        this.serviceStart('passive');
+        return;
+      }
+
+      // Check if service is ready to start in active mode
+      const serviceReady = await this.service.isServiceReadyToStart();
+      if (!serviceReady) {
+        console.log('Service not ready... DO NOT try to become leader...');
+        this.serviceStart('passive');
+        return;
+      }
+
+      // Get node address from seed
+      const key = await getKeysFromSeed(this.mnemonic);
+      const nodeKey = key.address;
+      debug('orchestrateService', `Current Node Key: ${nodeKey}`);
+
+      // Check if anyone is alive
+      if (!this.metrics.anyOneAlive(nodeKey, this.aliveTime)) {
+        console.log('Seems that no one is online...');
+        this.serviceStart('passive');
+        return;
+      }
+
+      // Check node leadership
+      console.log('Checking node leadership...');
+      const leadership = await this.leadershipManagement(nodeKey);
+
+      if (!leadership) {
+        console.log('The current node is not leader...');
+        this.serviceStart('passive');
+        return;
+      }
+
+      // If all checks passed we can start service in active mode
+      console.log('All checks passed. Launching service in active mode...');
+      await this.serviceStart('active');
     } catch (error) {
       debug('orchestrateService', error);
-      console.error(error);
+      throw error;
     }
-  };
+  }
+
+  // Take leader place
+  async becomeLeader (nodeKey) {
+    try {
+      const setLeader = await this.chain.setLeader(nodeKey, this.mnemonic); ;
+      if (setLeader) {
+        console.log('The leadership was taken successfully...');
+        return true;
+      } else {
+        console.log('Failed to take leadership. Possibly other node already took leadership...');
+        return false;
+      }
+    } catch (error) {
+      debug('becomeLeader', error);
+      throw error;
+    }
+  }
+
+  // Manage leadership
+  // Returns true if node is leader or was able to take leadership
+  async leadershipManagement (nodeKey) {
+    try {
+      // Get current leader from chain
+      let currentLeader = await this.chain.getLeader();
+      currentLeader = currentLeader.toString();
+
+      // If current leader is empty
+      // First time Archipel boot
+      if (currentLeader === '') {
+        console.log('Trying to take leadership...');
+        const becomeLeaderResult = await this.becomeLeader(nodeKey);
+        return becomeLeaderResult;
+      }
+
+      // If this node is current leader
+      if (currentLeader === nodeKey) {
+        console.log('Current node is leader...');
+        return true;
+      }
+
+      // Other node is leader
+      debug('orchestrateService', `Current Leader is: ${currentLeader}`);
+      console.log('Other node is leader...');
+
+      const otherLeaderActionResult = await this.otherLeaderAction(currentLeader, nodeKey);
+      return otherLeaderActionResult;
+    } catch (error) {
+      debug('leadershipManagement', error);
+      throw error;
+    }
+  }
 
   // Act if other node is leader
   async otherLeaderAction (currentLeader, nodeKey) {
@@ -85,22 +151,8 @@ class Orchestrator {
       // Get leader metrics known
       const leaderMetrics = this.metrics.getMetrics(currentLeader);
 
-      // If leader already sent an Metrics Update
-      if (leaderMetrics !== undefined) {
-        const nowTime = new Date().getTime();
-        const lastSeenAgo = nowTime - leaderMetrics.timestamp;
-
-        // Checking if leader can be considered alive
-        if (lastSeenAgo > this.aliveTime) {
-          await this.becomeLeader(currentLeader, nodeKey);
-        } else {
-          console.log(`Leader ${currentLeader} is alive no action required...`);
-          console.log('Enforcing passive mode...');
-          await this.serviceStart('passive');
-        }
-
-      // If there is no metrics received from leader node
-      } else {
+      // If no metrics received we will wait noLivenessThreshold
+      if (leaderMetrics === undefined) {
         // How much checks remains
         const checksNumber = this.noLivenessThreshold - this.noLivenessFromLeader;
 
@@ -109,87 +161,33 @@ class Orchestrator {
           console.log(`Will try to get leader place in ${checksNumber} checks...`);
           // Incrementing noLivenessFromLeader counter
           this.noLivenessFromLeader++;
-
+          return false;
         // No metrics received for noLivenessThreshold times. Leader is offline.
         } else {
-          console.log(`Can't check leader liveness for ${this.noLivenessThreshold} times.`);
-          await this.becomeLeader(currentLeader, nodeKey);
+          console.log(`Can't check leader ${currentLeader} liveness for ${this.noLivenessThreshold} times. Trying to become new leader...`);
+          this.noLivenessFromLeader = 0;
+          const becomeLeaderResult = await this.becomeLeader(nodeKey);
+          return becomeLeaderResult;
         }
+      }
+
+      const nowTime = new Date().getTime();
+      const lastSeenAgo = nowTime - leaderMetrics.timestamp;
+
+      // Checking if leader can be considered alive
+      if (lastSeenAgo > this.aliveTime) {
+        console.log(`Leader ${currentLeader} is down. Trying to become new leader...`);
+        const becomeLeaderResult =  await this.becomeLeader(nodeKey);
+        return becomeLeaderResult;
+      } else {
+        console.log(`Leader ${currentLeader} is alive...`);
+        return false;
       }
     } catch (error) {
       debug('otherLeaderAction', error);
       throw error;
     }
-  };
-
-  // Set leader on chain and launch service
-  async becomeLeader (oldLeaderKey, nodeKey) {
-    try {
-      if (!this.suspendService) {
-        console.log('Before trying to become leader, check if service is ready and safe to become leader...');
-        const isServiceReadyToStart = await this.isServiceReadyToStart();
-        if (isServiceReadyToStart) {
-          console.log('Trying to become leader...');
-          const leaderSet = await this.chain.setLeader(oldLeaderKey, this.mnemonic);
-          if (leaderSet === true) {
-            console.log('The leader set transaction was completed...');
-            console.log('Sleeping 5 seconds to be sure that transaction was propagated to every node...');
-            await new Promise(resolve => setTimeout(resolve, 5000));
-            await this.serviceStartIfAnyoneActive(nodeKey);
-          } else {
-            console.log('Can\'t set leader.');
-            console.log('Transaction failed or someone already took the leader place...');
-          }
-        } else {
-          console.log('Service not ready... DO NOT try to become leader...');
-        }
-      } else {
-        console.log('ARCHIPEL_SUSPEND_SERVICE is set to true... Only Passive mode. Leader will be never launched on this node.');
-      }
-    } catch (error) {
-      debug('becomeLeader', error);
-      throw error;
-    }
-  };
-
-  // Start active service is there is anyone online
-  async serviceStartIfAnyoneActive (nodeKey) {
-    try {
-      // The node is not isolated launching service in active mode
-      if (!this.suspendService && this.metrics.anyOneAlive(nodeKey, this.aliveTime)) {
-        console.log('Found someone online...');
-        console.log('Launching active node...');
-        await this.serviceStart('active');
-      // The node is isolated launching service in passive mode
-      } else {
-        console.log('Seems that no one is online...');
-        console.log('Launching passive node...');
-        await this.serviceStart('passive');
-      }
-    } catch (error) {
-      debug('currentLeader', error);
-      throw error;
-    }
-  };
-
-  // Check isServiceReadyToStart
-  async isServiceReadyToStart () {
-    try {
-      return await this.service.isServiceReadyToStart();
-    } catch (error) {
-      debug('isServiceReadyToStart', error);
-      throw error;
-    }
-  };
-
-  async serviceStart (mode) {
-    try {
-      await this.service.start(mode);
-    } catch (error) {
-      debug('serviceStart', error);
-      throw error;
-    }
-  };
+  }
 
   // Cleanup a service
   async serviceCleanUp () {
@@ -199,7 +197,7 @@ class Orchestrator {
       debug('serviceCleanUp', error);
       console.error(error);
     }
-  };
+  }
 }
 
 module.exports = {
