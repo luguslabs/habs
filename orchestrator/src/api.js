65cb61423e1f2212eed9bdea8c3c6443d9f4149d
Vladimir <vladost.pro@gmail.com>
2020-03-18 10:22:56 +0100

    [orchestrato] Show all info in API

--- orchestrator/src/api.js
+++ orchestrator/src/api.js
@@ -4,27 +4,49 @@ const bodyParser = require('body-parser');
 
 // Get orchestrator info
 const getOrchestratorInfo = async orchestrator => {
-    const ret = {
-        status: 'ok',
-        mode: orchestrator.mode,
-        suspendService: orchestrator.suspendService
-    }
 
-    return ret;
+  // Get all necessary orchestrator data
+  const metrics = orchestrator.metrics.getAllMetrics();
+  const orchestratorAddress = await orchestrator.getOrchestratorAddress();
+  const isConnected = orchestrator.chain.isConnected();
+  const peerId = await orchestrator.chain.getPeeId();
+  const peerNumber = await orchestrator.chain.getPeerNumber();
+  const synchState = await orchestrator.chain.getSyncState();
+  const currentLeader = await orchestrator.chain.getLeader();
+  const isServiceReadyToStart = await orchestrator.isServiceReadyToStart();
+  const launchedContainer = await orchestrator.service.checkLaunchedContainer();
+
+  // Constructing info object
+  const result = {
+      status: '200',
+      orchestratorAddress: orchestratorAddress,
+      isConnected: isConnected,
+      peerId: peerId,
+      peerNumber: peerNumber,
+      synchState: synchState,
+      leader: currentLeader,
+      service: orchestrator.serviceName,
+      suspendService: orchestrator.suspendService,
+      isServiceReadyToStart: isServiceReadyToStart,
+      serviceMode: orchestrator.mode,
+      serviceContainer: launchedContainer,
+      metrics: metrics
+  }
+  return result;
 }
 
 // Return not found response
 const get404 = (req, res, next) => {
-    const error = {
-      errors: [
-        {
-          status: '404',
-          title: 'Not found',
-          detail: 'Requested resource was not found.'
-        }
-      ]
-    };
-    res.status(404).json(error);
+  const error = {
+    errors: [
+      {
+        status: '404',
+        title: 'Not found',
+        detail: 'Requested resource was not found.'
+      }
+    ]
+  };
+  res.status(404).json(error);
 };
 
 // Simple error handler
@@ -41,6 +63,7 @@ const errorHandler = (err, req, res, next) => {
     res.status(500).json(error);
 };
 
+// Init api
 const initApi = async orchestrator => {
     const app = express();
 
@@ -49,7 +72,10 @@ const initApi = async orchestrator => {
     
     // Add main route
     app.get('/', asyncHandler(async (req, res) => {
+        // Get orchestrator info object
         const ret = await getOrchestratorInfo(orchestrator);
+
+        // Return object as json
         res.json(ret);
     }));
 
