4f9017145e66c591db52efb7190d5d6e9ebc30fa
Vladimir <vladost.pro@gmail.com>
2021-07-19 12:09:46 +0200

    Some tests cleaning

--- deployer/test/restart-archipel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Starting archipel $1..."
-docker start archipel$1
-
-docker ps 
--- deployer/test/stop-archipel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Stopping archipel $1..."
-docker stop archipel$1
-
-docker ps
