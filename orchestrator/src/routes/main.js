53292fc299b8272283652245ffaa46d215e46133
Vladimir <vladost.pro@gmail.com>
2021-07-28 16:58:16 +0200

    Little change in return get orchestrator info api

--- orchestrator/src/routes/main.js
+++ orchestrator/src/routes/main.js
@@ -7,42 +7,26 @@ const { getKeysFromSeed } = require('../utils');
 
 // Get orchestrator info
 const getOrchestratorInfo = async orchestrator => {
-  // Get all necessary orchestrator data
-  const heartbeats = orchestrator.heartbeats.getAllHeartbeats();
-
-  const getOrchestratorKey = await getKeysFromSeed(orchestrator.mnemonic);
-  const orchestratorAddress = getOrchestratorKey.address;
-
-  const isConnected = orchestrator.chain.isConnected();
-  const peerId = await orchestrator.chain.getPeerId();
-  const peerNumber = await orchestrator.chain.getPeerNumber();
-  const bestNumber = await orchestrator.chain.getBestNumber();
-  const synchState = await orchestrator.chain.getSyncState();
-  const currentLeader = await orchestrator.chain.getLeader(orchestrator.group);
-  const isServiceReadyToStart = await orchestrator.service.serviceReady();
-  const launchedContainer = await orchestrator.service.serviceCheck();
-
   // Constructing info object
-  const result = {
+  return {
     status: '200',
-    orchestratorAddress: orchestratorAddress,
+    orchestratorAddress: (await getKeysFromSeed(orchestrator.mnemonic)).address,
     archipelName: orchestrator.archipelName,
-    isConnected: isConnected,
-    peerId: peerId,
-    peerNumber: peerNumber,
-    bestNumber: bestNumber,
-    synchState: synchState,
-    leader: currentLeader,
+    isConnected: orchestrator.chain.isConnected(),
+    peerId: await orchestrator.chain.getPeerId(),
+    peerNumber: await orchestrator.chain.getPeerNumber(),
+    bestNumber: await orchestrator.chain.getBestNumber(),
+    synchState: await orchestrator.chain.getSyncState(),
+    leader: await orchestrator.chain.getLeader(orchestrator.group),
     service: orchestrator.service.serviceName,
     orchestrationEnabled: orchestrator.orchestrationEnabled,
-    isServiceReadyToStart: isServiceReadyToStart,
+    isServiceReadyToStart: await orchestrator.service.serviceReady(),
     serviceMode: orchestrator.service.mode,
-    serviceContainer: launchedContainer,
+    serviceContainer: await orchestrator.service.serviceCheck(),
     heartbeatSendEnabledAdmin: orchestrator.heartbeatSendEnabledAdmin,
     heartbeatSendEnabled: orchestrator.heartbeatSendEnabled,
-    heartbeats: heartbeats
+    heartbeats: orchestrator.heartbeats.getAllHeartbeats()
   };
-  return result;
 };
 
 // Show main page route
